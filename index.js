88e75d1656d71d7a7631f48107af95a9a52d5d29
Notarin Steele <424c414e4b@gmail.com>
2019-05-13 15:06:30 -0400

    more things

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ client.on("message", message => {
   const command = args.shift().toLowerCase();
   //help command
   if (command === 'help') {
-    message.reply('say ' + conf.prefix + 'yandere for a random post from yande.re')
-    message.reply('say ' + conf.prefix + 'konachan for a random post from konachan.com')
-    message.reply('say ' + conf.prefix + 'danbooru for a random post from danbooru.donmai.us')
+  message.reply('no help setup yet')
   }
   //test command to see both the command and arguments
   if (command === 'test') {
@@ -54,67 +52,6 @@ client.on("message", message => {
           message.channel.send(data['0'].sample_url)
         } else {
           message.channel.send("no post found:(")
-        }
-      });
-  }
-  //pulls post from konachan
-  if (command === 'konachan') {
-    switch (message.channel.nsfw) {
-      //auto nsfw
-      case true:
-        var rating = "e";
-        console.log("nsfw");
-        break;
-      case false:
-        var rating = "s";
-        console.log("sfw");
-        break;
-    }
-    console.log("request recieved");
-    message.reply('request recieved');
-    //changes aruments into a string
-    let strargs = args.toString();
-    //replaces every comma into a plus in the string
-    let newargs = strargs.replace(/,/g, '+');
-    //pulls post from api and sends it
-    require("request")("https://konachan.com/post.json?limit=1&tags=order%3Arandom+rating%3A" + rating + "+" + newargs,
-      function(err, res, body) {
-        let data = JSON.parse(body);
-        if (data['0'] !== undefined) {
-          message.channel.send(data['0'].sample_url)
-        } else {
-          message.channel.send("no post found:(")
-        }
-      });
-  }
-  //pulls post from danbooru
-  if (command === 'danbooru') {
-    switch (message.channel.nsfw) {
-      //auto nsfw
-      case true:
-        var rating = "e";
-        console.log("nsfw");
-        break;
-      case false:
-        var rating = "s";
-        console.log("sfw");
-        break;
-    }
-    console.log("request recieved");
-    message.reply('request recieved');
-    //changes aruments into a string
-    let strargs = args.toString();
-    //replaces every comma into a plus in the string
-    let newargs = strargs.replace(/,/g, '+');
-    //pulls post from api and sends it
-    require("request")("https://danbooru.donmai.us/posts.json?limit=1&tags=order%3Arandom+rating%3A" + rating + "+" + newargs,
-      function(err, res, body) {
-        let data = JSON.parse(body);
-        if (data['0'] !== undefined) {
-          message.channel.send(data['0'].file_url)
-        } else {
-          // danbooru restrics its api to two tags
-          message.channel.send("either you searched with too many terms or there was no post found")
         }
       });
   }
