0ffe6686614555b6197b4036d78cfe911a4c211e
Notarin <424c414e4b@gmail.com>
2019-05-22 09:30:36 -0400

    Removed eval command

--- index.js
+++ index.js
@@ -55,22 +55,6 @@ client.on("message", message => {
         }
       });
   }
-  if (message.content.startsWith(config.prefix + "eval")) {
-    if (message.author.id !== config.ownerID) return;
-    try {
-      const code = args.join(" ");
-      let evaled = eval(code);
-
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-
-      message.channel.send(clean(evaled), {
-        code: "xl"
-      });
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-  }
 });
 
 //logs in with token
