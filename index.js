2f7973912b0b135246714552c3e6fd3483434e41
Your Name <you@example.com>
2019-10-19 20:30:55 -0400

    Fixed some things

--- index.js
+++ index.js
@@ -75,13 +75,10 @@ client.on("message", message => {
   }
   if (command === 'shitpost') {
     var rand = Math.floor(Math.random() * (3 - 1 + 1) + 1);
-<<<<<<< HEAD
     var num = sp[rand]
     message.channel.send(num);
-=======
     console.log(sp[rand]);
     message.channel.send(sp[rand]);
->>>>>>> 08c187271cab7b3f3652a43af9131afe05513557
   }
   //commands end here
 });
